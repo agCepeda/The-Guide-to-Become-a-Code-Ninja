cc08fc1676dd9a64f80f23ff97caeeda32e93ddf
David Rodriguez Reveles <davidrodr95@gmail.com>
2017-06-29 10:56:49 -0500

    Strategy pattern

---
+++ strategy/strategy.php
@@ -0,0 +1,75 @@
+<?php
+interface Estrategia
+{
+    public function movilizar();
+}
+
+class MoverLento implements Estrategia
+{
+    public function movilizar()
+    {
+        echo 'Moviendo unidades lento';
+    }
+}
+
+class MoverNormal implements Estrategia
+{
+    public function movilizar()
+    {
+        echo 'Moviendo unidades normal';
+    }
+}
+
+class MoverRapido implements Estrategia
+{
+    public function movilizar()
+    {
+        echo 'Moviendo unidades rápido';
+    }
+}
+
+abstract class Raza
+{
+    private $estrategia;
+
+    public function setEstrategia($estrategia)
+    {
+        $this->estrategia = $estrategia;
+    }
+
+    public function moverUnidades()
+    {
+        $this->estrategia->movilizar();
+    }
+}
+
+class Zerg extends Raza
+{
+}
+
+class Terran extends Raza
+{
+}
+
+class Protoss extends Raza
+{
+}
+
+$moverLento = new MoverLento;
+$moverNormal = new MoverNormal;
+$moverRapido = new MoverRapido;
+
+echo "Protoss: ";
+$zerg = new Protoss;
+$zerg->setEstrategia($moverLento);
+$zerg->moverUnidades();
+
+echo "<br>Terran: ";
+$zerg = new Terran;
+$zerg->setEstrategia($moverNormal);
+$zerg->moverUnidades();
+
+echo "<br>Zerg: ";
+$zerg = new Zerg;
+$zerg->setEstrategia($moverRapido);
+$zerg->moverUnidades();
